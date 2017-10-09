7bb295adf28ace3db9b9529b6fb972059deff60c
iNexus The Creator <supp.alliedsource@gmail.com>
2017-10-09 16:55:36 +0300

    Add files via upload

---
+++ GetLastDigitConvertToWord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = int.Parse(Console.ReadLine());
+            int LastNumber = (Number % 10);
+
+            switch(LastNumber)
+                {
+                case 1: Console.WriteLine("one");break;
+                case 2: Console.WriteLine("two"); break;
+                case 3: Console.WriteLine("three"); break;
+                case 4: Console.WriteLine("four"); break;
+                case 5: Console.WriteLine("five"); break;
+                case 6: Console.WriteLine("six"); break;
+                case 7: Console.WriteLine("seven"); break;
+                case 8: Console.WriteLine("eight"); break;
+                case 9: Console.WriteLine("nine"); break;
+                case 10: Console.WriteLine("ten"); break;
+            }
+        }
+    }
+}
---
+++ GetMaxNumberOfThree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MaxNumber;
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+
+            if (c > GetMax(a, b)) MaxNumber = c;
+            else MaxNumber = GetMax(a, b);
+
+            Console.WriteLine(MaxNumber);
+        }
+        private static int GetMax(int a, int b)
+        {
+            int MAX;
+
+            if (a > b) MAX = a;
+            else MAX = b;
+
+            return MAX;
+        }
+    }
+}
+
