d6942562c6417ed52d0d18dba06ed47148c8e815
iNexus The Creator <supp.alliedsource@gmail.com>
2017-10-14 22:35:43 +0300

    Add files via upload

---
+++ Words.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+            var input = "Java C# PHP PHP JAVA C java";
+            var words = input.ToLower().Split(' ');
+            var counts = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                counts[word] = 0 ;
+            }
+            foreach (var word in words)
+            {
+                counts[word]++;
+            }
+
+            Console.WriteLine(String.Join(", ", counts));
+        }
+}
+}
