36d54ad2439fb4c24688a94a6be798b21bac38ed
iNexus The Creator <supp.alliedsource@gmail.com>
2017-10-22 17:16:01 +0300

    Add files via upload

---
+++ GetFolderSize.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+static void Main(string[] args)
+{
+    string[] files = Directory.GetFiles("../../TestFolder");
+    double Sum = 0;
+
+    foreach (string file in files)
+    {
+        FileInfo fileInfo = new FileInfo(file);
+        Sum += fileInfo.Length;
+    }
+
+        Sum = Sum / 1024 / 1024; // byte to MB
+        File.WriteAllText("../../result.txt", Sum.ToString());
+}
+}
+    
+
