ea6a7e0367cf986d4b7d581622c7040dfa0b0901
iNexus The Creator <supp.alliedsource@gmail.com>
2017-10-22 15:26:07 +0300

    Add files via upload

---
+++ GetOdd_Lines_fromFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+static void Main(string[] args)
+{
+
+
+       string[] lines = File.ReadAllLines("input.txt");
+        string[] oddlines = lines.Where((line, i) => i % 2 == 1).ToArray();
+
+        File.WriteAllLines("output.txt", oddlines);
+
+    }
+
+}
+    
+
