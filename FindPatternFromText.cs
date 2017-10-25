c17f9a6de81232e6c4cff6ab18715dbacce6da25
iNexus The Creator <supp.alliedsource@gmail.com>
2017-10-25 13:23:52 +0300

    Add files via upload

---
+++ FindPatternFromText.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+static void Main(string[] args)
+{
+
+        var text = Console.ReadLine();
+        var pattern = Console.ReadLine();
+
+        var count = 0;
+        var index = -1;
+
+        while(true)
+        {
+            index = text.IndexOf(pattern, index + 1);
+
+            if (index == -1)
+                break;
+
+            count++;
+
+        }
+        Console.WriteLine(count);
+
+    }
+}
+    
+
