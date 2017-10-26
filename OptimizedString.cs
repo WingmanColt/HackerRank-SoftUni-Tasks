9dd8d8cb5cb35a792778ba56499b225e4828cf42
iNexus The Creator <supp.alliedsource@gmail.com>
2017-10-26 16:45:17 +0300

    Add files via upload

---
+++ OptimizedString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var str = new StringBuilder();
+        var num = decimal.Parse(Console.ReadLine());
+            
+        for (int i = 0; i < num; i++)
+        {
+            str.Append("Тест");
+        }
+
+        Console.WriteLine(str.ToString().Length);
+    }
+}
+}
