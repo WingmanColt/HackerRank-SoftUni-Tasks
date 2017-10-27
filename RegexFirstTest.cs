65a05d723fa9d92ec2e52c335995871ce6894f33
iNexus The Creator <supp.alliedsource@gmail.com>
2017-10-27 15:39:02 +0300

    Add files via upload

---
+++ RegexFirstTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        var text = @"https://www.cms-bg.eu/youtube 
+                    drugtext test
+                    http://www.shop.cms-bg.eu/";
+        var pattern = @"(\w+)\:\/\/([a-z0-9._-]+)(.*)";
+
+        foreach(Match match in Regex.Matches(text, pattern))
+        {
+            Console.WriteLine("Protocol: " + match.Groups[1]);
+            Console.WriteLine("Host: " + match.Groups[2]);
+            Console.WriteLine("Resources: " + match.Groups[3]);
+        }
+
+
+    }
+}
+}
