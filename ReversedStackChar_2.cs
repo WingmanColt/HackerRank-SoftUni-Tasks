b43077195a2229b12772a0c1e060f8730407aff9
iNexus The Creator <supp.alliedsource@gmail.com>
2018-09-22 05:39:33 -0700

    Add files via upload

---
+++ ReversedStackChar_2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stringReverser = new Stack<char>();
+            
+            string input = Console.ReadLine();
+
+            foreach (var ch in input)
+            {
+                stringReverser.Push(ch);
+            }
+
+            while (stringReverser.Count > 0)
+            {
+                Console.Write(stringReverser.Pop());
+            }
+        }
+    }
+}
